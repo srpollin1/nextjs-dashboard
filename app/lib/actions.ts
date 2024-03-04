b8cb106f09b0af7f6c5e0cfffc6bdd34e7b4d12c
Eduardo Retamales <eduardoretamalesrojas@gmail.com>
2024-03-04 10:24:29 -0300

    add invoice functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,10 @@
 'use server';
-
-import { z } from 'zod';
  
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -10,16 +13,22 @@ const FormSchema = z.object({
   date: z.string(),
 });
  
-// const CreateInvoice = FormSchema.omit({ id: true, date: true });
-
-
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
  
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // Test it out:
-  console.log(rawFormData);
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+ 
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
